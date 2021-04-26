4603fb510c63ae97c2d0b1073ea78baf3ca61bba
abhishek36 <akamble9913@gmail.com>
2021-04-26 12:51:41 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,11 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/storage'
+var firebaseConfig = {
+  apiKey: "AIzaSyCm_7_MTGwpQTa8fg4vkCDIUdRtBnsFGKE",
+  authDomain: "evernote-1939a.firebaseapp.com",
+  projectId: "evernote-1939a",
+  storageBucket: "evernote-1939a.appspot.com",
+  messagingSenderId: "285205193499",
+  appId: "1:285205193499:web:06002b1e312670baf4e9be"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('evernote-container')
 );
 
 // If you want to start measuring performance in your app, pass a function
